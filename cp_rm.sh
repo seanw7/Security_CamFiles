2ac115edfe0b9f6bb448927ac3f07a9b5422e278
seanw7 <seanw@protonmail.ch>
2017-09-16 14:03:20 -0700

    Made initial commit of cam2 branch for second picamera. Modified cp_rm.sh to have cam2 in the folder dest name.

--- cp_rm.sh
+++ cp_rm.sh
@@ -2,6 +2,6 @@
 d=$(date +%Y-%m-%d)
 echo "$d"
 
-rsync -r -v ~/pi-timolo/media/motion/*.jpg 192.168.0.46:~/Pictures/cam1_$d/
+rsync -r -v ~/pi-timolo/media/motion/*.jpg 192.168.0.46:~/Pictures/cam2_$d/
 cd ~/pi-timolo/media/motion/
 find -name '*.jpg' -delete
