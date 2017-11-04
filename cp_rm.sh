220096e10f3c7183c06b617fe77fe6d30ba306aa
seanw7 <seanw@protonmail.ch>
2017-11-04 13:09:27 -0700

    Fixed script for cam 2

--- cp_rm.sh
+++ cp_rm.sh
@@ -1,3 +1,4 @@
+  GNU nano 2.2.6                                    File: cp_rm
 #!/bin/bash
 full_date=$(date +%Y-%m-%d)
 y=$(date +%Y)
@@ -9,4 +10,12 @@ echo "$full_date"
 echo "attempting to sync motion photos to backup location"
 rsync -ramP '/home/pi/pi-timolo/media/motion/' pi@192.168.0.46:/home/pi/Pictures/cam2/$full_date/
 echo "completed moving photos..."
-#find -name '*.jpg' -delete
+find -/pi-timolo/media/motion/*.jpg -delete
+
+#cd ~/pi-timolo/media/timelapse/
+echo "attempting to sync motion photos to backup location"
+rsync -ramP '/home/pi/pi-timolo/media/timelapse/' pi@192.168.0.46:/home/pi/Pictures/cam2-timelapse/$full_date/
+echo "completed moving photos..."
+#find ~/pi-timolo/media/timelapse/*.jpg -delete
+find ~/pi-timolo/media/timelapse/*.jpg
+
