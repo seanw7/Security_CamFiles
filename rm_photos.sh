ce8385e2782f4dbce906f95c9cf9a3dc4ad617e3
seanw7 <seanw@protonmail.ch>
2017-11-04 13:14:43 -0700

    Modified the rm_photos script

--- rm_photos.sh
+++ rm_photos.sh
@@ -2,7 +2,11 @@
 
 # deletes pi-timolo motion photos
 
-cd ~/pi-timolo/media/motion/
+#cd ~/pi-timolo/media/motion/
 echo "Deleting jpg files @ /pi-timolo/media/motion/"
-find -name '*.jpg' -delete
-echo "Finished Deleting files..."
+find ~/pi-timolo/media/motion/*.jpg -delete
+echo "Finished Deleting Motion files..."
+find ~/pi-timolo/media/timelapse/*.jpg -delete
+echo "Finished Deleting TimeLapse files..."
+
+
