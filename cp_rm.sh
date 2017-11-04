74eec1a91941b10cf12eff8286db220620825833
seanw7 <seanw@protonmail.ch>
2017-11-04 11:16:17 -0700

    modified cp_rm script to also copy over and delete the timelapse photos

--- cp_rm.sh
+++ cp_rm.sh
@@ -9,4 +9,12 @@ echo "$full_date"
 echo "attempting to sync motion photos to backup location"
 rsync -ramP '/home/pi/pi-timolo/media/motion/' pi@192.168.0.46:/home/pi/Pictures/cam1/$full_date/
 echo "completed moving photos..."
-#find -name '*.jpg' -delete
+find ~/pi-timolo/media/motion/*.jpg -type f
+
+#cd ~/pi-timolo/media/timelapse/
+echo "attempting to sync timelapse photos to backup location"
+rsync -ramP '/home/pi/pi-timolo/media/timelapse/' pi@192.168.0.46:/home/pi/Pictures/cam1-timelapse/$full_date/
+echo "completed moving photos..."
+find ~/pi-timolo/media/timelapse/*.jpg -type f
+
+
